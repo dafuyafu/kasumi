bc986ce8d52389ebb1d7718e7b852ac102eacb24
dafuyafu <dafuyafu@MacBook-Pro.local>
2021-02-25 10:05:06 +0900

    update basics.py: add subs() and add Symbol class

--- basics.py
+++ basics.py
@@ -1,3 +1,59 @@
+class Symbol:
+	def __init__(self, char):
+		if not isinstance(char, str):
+			raise TypeError("can generate Symbol only with str, not %s", char.__class__.__name__)
+		self.char = char
+
+	def __repr__(self):
+		return self.char
+
+	def __str__(self):
+		return self.char
+
+	def __add__(a, b):
+		if isinstance(b, Symbol):
+			return a.as_dup() + b.as_dup()
+		else:
+			return a.as_dup() + b
+
+	def __sub__(a, b):
+		if isinstance(b, Symbol):
+			return a.as_dup() - b.as_dup()
+		else:
+			return a.as_dup() - b
+
+	def __mul__(a, b):
+		if isinstance(b, Symbol):
+			return a.as_dup() * b.as_dup()
+		else:
+			return a.as_dup() * b
+
+	def __radd__(a, b):
+		return a + b
+
+	def __rsub__(a, b):
+		return - a + b
+
+	def __rmul__(a, b):
+		return a * b
+
+	def __neg__(a):
+		return a * -1
+ 
+	def as_dup(self):
+		return dup(self, [0, 1])
+
+def symbol(c):
+	if not isinstance(c, str):
+		raise TypeError("symbol of variable must be str, not %s", c.__class__.__name__)
+	return Symbol(c)
+
+def symbols(c):
+	if not isinstance(c, str):
+		raise TypeError("symbol of variable must be str, not %s", c.__class__.__name__)
+	symbols_ = c.replace(" ", "").split(",")
+	return tuple([symbol(s) for s in symbols_])
+
 class DUP:
 	"""
 
@@ -5,72 +61,31 @@ class DUP:
 	Coefficients need to be integers.
 
 	"""
+
+
 	def __init__(self, symbol, coeffs=[]):
-		self.symbol = symbol
-		self.coeffs = []
-		self.unset = True
-		if not isinstance(coeffs, list):
-			raise TypeError("needs to be list")
+		if not isinstance(symbol, Symbol):
+			raise TypeError("symbol must be Symbol, not %s", symbol.__class__.__name__)
+		elif not isinstance(coeffs, list):
+			raise TypeError("coefficients must be list, not %s", coeffs.__class__.__name__)
 		else:
+			self.symbol = symbol
 			if coeffs == []:
-				pass
+				self.set([0])
 			else:
 				self.set(coeffs)
 
 	def set(self, coeffs):
-		if self.unset:
-			self.coeffs = coeffs
-			self.deg = len(coeffs) - 1
-			self.trdeg = self.trailing_deg()
-			self.unset = False
-		else:
-			raise ValueError("Already set.")
+		self.coeffs = coeffs
+		self.deg = len(coeffs) - 1
+		self.trdeg = self.trailing_deg()
+		self.unset = False
 
 	def __repr__(self):
 		return self.sparse_rep()
 
 	def __str__(self):
 		return self.sparse_rep()
-
-	def trailing_deg(self):
-		for d in range(self.deg + 1):
-			if self.coeffs[d] != 0:
-				return d
-
-	def sparse_rep(self):
-		rep = ""
-		deg = self.deg
-		while deg >= 0:
-			c = self.coeffs[deg]
-			if isinstance(c, DUP) and c.deg > 0:
-				if deg > 0:
-					c = "(" + str(c) + ")"
-				else:
-					c = str(c)
-			else:
-				c = str(abs(c))
-			if deg > 1:
-				if c != "1":
-					rep += c + "*"
-				rep += self.symbol + "**" + str(deg)
-			elif deg == 1:
-				if c != "1":
-					rep += c + "*"
-				rep += self.symbol
-			else:
-				rep += c
-			if deg != self.trdeg:
-				while True:
-					deg -= 1
-					if self.coeffs[deg] != 0:
-						break
-				if self.coeffs[deg] > 0:
-					rep += " + "
-				else:
-					rep += " - "
-			else:
-				break
-		return rep
 
 	"""
 	Binary operations:
@@ -81,7 +96,7 @@ class DUP:
 		if isinstance(g, DUP):
 			if g.symbol == f.symbol:
 				m = min(g.deg, f.deg) + 1
-				coeffs_ = [f.coeffs[i] + f.coeffs[i] for i in range(m)]
+				coeffs_ = [f.coeffs[i] + g.coeffs[i] for i in range(m)]
 				if f.deg > g.deg:
 					coeffs_ += f.coeffs[m:]
 				else:
@@ -94,8 +109,14 @@ class DUP:
 			coeffs_ = [f.coeffs[0] + g] + f.coeffs[1:]
 			return dup(f.symbol, coeffs_)
 
+	def __radd__(f, g):
+		return f + g
+
 	def __sub__(f, g):
 		return f + g * (-1)
+
+	def __rsub__(f, g):
+	 	return - (f - g)
 
 	def __mul__(f, g):
 		if isinstance(g, DUP):
@@ -112,11 +133,15 @@ class DUP:
 					coeffs_.append(co_)
 				return dup(f.symbol, coeffs_)
 			else:
-				# return DMP
+				var = (f.symbol, g.symbol)
+
 				pass
 		if isinstance(g, int):
 			coeffs_ = [f.coeffs[i] * g for i in range(f.deg + 1)]
 			return dup(f.symbol, coeffs_)
+
+	def __rmul__(f, g):
+		return f * g
 
 	def __eq__(f, g):
 		if isinstance(g, DUP):
@@ -133,21 +158,161 @@ class DUP:
 	def __gt__(f, g):
 		return True
 
+	def __mod__(f, g):
+		if isinstance(g, int):
+			return dup(f.symbol, [c % g for c in f.coeffs]) 
+		else:
+			raise NotImplementedError()
+
+	def __neg__(f):
+		return f * -1
+
+	def trailing_deg(self):
+		for d in range(self.deg + 1):
+			if self.coeffs[d] != 0:
+				return d
+		return None
+
+	def sparse_rep(self):
+		rep = ""
+		deg = self.deg
+		while deg >= 0:
+			c = self.coeffs[deg]
+			if isinstance(c, DUP) and c.deg > 0:
+				if deg > 0:
+					c = "(" + str(c) + ")"
+				else:
+					c = str(c)
+			else:
+				c = str(abs(c))
+			if deg > 1:
+				if c != "1":
+					rep += c + "*"
+				rep += str(self.symbol) + "**" + str(deg)
+			elif deg == 1:
+				if c != "1":
+					rep += c + "*"
+				rep += str(self.symbol)
+			else:
+				rep += c
+			if deg != self.trdeg:
+				while True:
+					deg -= 1
+					if self.coeffs[deg] != 0:
+						break
+				if self.coeffs[deg] > 0:
+					rep += " + "
+				else:
+					rep += " - "
+			else:
+				break
+		return rep
+
+	def isconstant(self):
+		if self.deg == 0:
+			return True
+		else:
+			return False
+
+	def degree(self):
+		return self.deg
+
+	def coeffs_list(self):
+		coeffs_ = []
+		for c in self.coeffs:
+			if isinstance(c, DUP):
+				coeffs_.append(c.coeffs_list())
+			else:
+				coeffs_.append(c)
+		return coeffs_
+
+	def inner_vars(self):
+		vars_ = (self.symbol, )
+		for c in self.coeffs:
+			if isinstance(c, DUP):
+				vars_ += c.inner_vars()
+			else:
+				continue
+		return vars_
+
+	def subs(self, subsdict):
+		if not isinstance(subsdict, dict):
+			raise TypeError("argument must be dict, not %s", subsdict.__class__.__name__)
+		if not self.symbol in subsdict:
+			return self
+		else:
+			subs_ = 0
+			value_ = subsdict[self.symbol]
+			for i in range(self.deg + 1):
+				subs_ += self.coeffs[i] * value_ ** i
+			return subs_
+
+def dup(symbol, coeffs):
+	return DUP(symbol, coeffs)
+
+def dup_from_dict(var, rep):
+	if not isinstance(var, tuple):
+		raise TypeError("first argument must be tuple, not %s", var.__class__.__name__)
+	if len(var) == 0:
+		raise ValueError("needs one variable at least")
+	if len(var) == 1:
+		coeffs_, i, j = [], 0, 0
+		while i < len(rep):
+			try:
+				coeffs_.append(rep[j])
+				i += 1
+			except KeyError:
+				coeffs_.append(0)
+			finally:
+				j += 1
+		return dup(var[0], coeffs_)
+	else:
+		pass
+
+def dup_from_list(var, rep):
+	if not isinstance(var, tuple):
+		raise TypeError("first argument must be tuple, not %s", var.__class__.__name__)
+	if len(var) == 0:
+		raise ValueError("needs one variable at least")
+	if len(var) == 1:
+		return dup(var[0], rep)
+	else:
+		coeffs_ = []
+		for r in rep:
+			if isinstance(r, list):
+				coeffs_.append(dup_from_list(var[1:], r))
+			elif isinstance(r, int):
+				coeffs_.append(r)
+			else:
+				raise ValueError("elements of coeffs list must be int or DUP, not %s", r.__class__.__name__)
+		return dup(var[0], coeffs_)
+
 class DMP:
 	def __init__(self, symbols, rep):
 		if not isinstance(rep, DUP):
-			raise TypeError("needs to be a DUP object")
+			raise TypeError("rep must be DUP, not %s", rep.__class__.__name__)
 		self.symbols = symbols
 		self.rep = rep
 
-def dup(s, co):
-	return DUP(s, co)
+	def __repr__(self):
+		return self.rep.sparse_rep()
 
-def expr(rep):
-	return Expr(rep)
+	def __str__(self):
+		return self.rep.sparse_rep()
 
-def symbol(c):
-	return expr(dup(c, [0,1]))
+	def variable_sort(self, t):
+		if not isinstance(t, tuple):
+			raise TypeError("needs one tuple not %s", t.__class__.__name__)
+		if not set(self.symbols) == set(t):
+			raise ValueError("can sort only if both polynomial have the same variables")
+		if self.symbols == t:
+			raise ValueError("already sorted")
+
+	def degree(self, *var):
+		pass
+
+	def subs(self, subsdict):
+		pass
 
 def abs(rep):
 	if isinstance(rep, int):
